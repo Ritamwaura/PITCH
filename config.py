e4895066b3c69899de0568ce0231765334140e73
Your Name <mwaurarita2019@gmail.com>
2019-09-17 16:33:00 +0300

    deployment

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY='1234'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+   # SECRET_KEY='1234'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rita:1234@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
