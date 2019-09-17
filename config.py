2cd3238c1a765553a201983d1ed31501e00beae4
Your Name <mwaurarita2019@gmail.com>
2019-09-17 17:28:57 +0300

    deployment

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class TestConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rita:1234@localhost/pitch'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rita:1234@localhost/pitch'
 
 class DevConfig(Config):
     '''
@@ -41,10 +41,10 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rita:1234@localhost/pitch'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rita:1234@localhost/pitch'
 
     DEBUG = True
-    ENV = 'development'
+    #ENV = 'development'
     
 config_options = {
 'development':DevConfig,
